c615a829fd9cd936eec6708349b8fb679af2d8f5
Cryptoking94 <98610415+Cryptoking94@users.noreply.github.com>
2024-08-19 15:28:03 +0530

    Create main.tf

---
+++ modules/ec2/main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "testing_instance" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  associate_public_ip_address = true
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = var.security_group_ids  # Use vpc_security_group_ids instead of security_group_ids
+  key_name               = var.key_pair_name
+}
+
+output "public_ip" {
+  value = aws_instance.testing_instance.public_ip
+}
